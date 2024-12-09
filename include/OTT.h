5303ff24970fb98a630cf3c74ea9d04d88aa287c
Phúc LĐ <phucld@gviet.vn>
2024-12-09 09:55:13 +0700

    docs: update sdk support save log file

--- include/OTT.h
+++ include/OTT.h
@@ -11,8 +11,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSInteger, SmLogLevel) {
+    SM_LOG_LEVEL_NONE = 0,
+    SM_LOG_LEVEL_ERROR,
+    SM_LOG_LEVEL_WARNING,
+    SM_LOG_LEVEL_INFO,
+    SM_LOG_LEVEL_DEBUG
+};
+
 @interface OTT : NSObject
 +(OTT *)getInstance;
+
++ (void) setLogLevel:(SmLogLevel) logLevel;
++ (SmLogLevel) getLogLevel;
+
 -(void)initialize:(NSString *)clientKey;
 -(NSString *)generate:(NSString *)url configKey:(NSString *)configKey;
 @end
--- include/SmMonitor.h
+++ include/SmMonitor.h
@@ -3,16 +3,13 @@
 #import "SmCustomData.h"
 
 @interface SmMonitor : NSObject
-
 + (instancetype)sharedInstance;
 
-- (void) setDebug:(bool) debug;
 - (void) attachAVPlayer:(nonnull AVPlayer *)player withAVPlayerLayer:(nonnull AVPlayerLayer*) playerLayer;
 - (void) attachAVPlayer:(nonnull AVPlayer *)player withAVPlayerViewController:(nonnull AVPlayerViewController *)playerViewController;
 - (void) setCustomData:(SmCustomData *)customData;
 - (void) recordAppStartTime;
 - (double) timeSinceAppStart;
-
 
 - (void) destroy;
 @end
